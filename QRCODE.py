4e871abdddc78d97dd7de9e4435150870bb3c2f6
yanis26x <yanis.djenadi@hotmail.com>
2025-03-11 09:51:00 -0400

    generateur QR avec couleur

--- QRCODE.py
+++ QRCODE.py
@@ -1,5 +1,16 @@
-import qrcode
+import qrcode.constants
 
-img = qrcode.make('https://github.com/yanis26x')
+
+qr = qrcode.QRCode(
+    #entre 1 et 40
+    version=3,
+    box_size=3,
+    border=5
+
+)
+
+qr.add_data('https://github.com/yanis26x')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="blue")
 img.save('qrcode26x.png')
-
